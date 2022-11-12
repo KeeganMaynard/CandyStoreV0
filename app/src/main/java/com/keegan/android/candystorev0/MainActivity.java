80a4c13a1935dc1643cb97b795783012a948b2d0
Keegan <kgm1328@live.missouristate.edu>
2022-11-12 11:44:53 -0600

    Completed implementation of V0

--- app/src/main/java/com/keegan/android/candystorev0/MainActivity.java
+++ app/src/main/java/com/keegan/android/candystorev0/MainActivity.java
@@ -1,8 +1,12 @@
 package com.keegan.android.candystorev0;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        switch ( id ) {
+            case R.id.action_add:
+                Log.w( "MainActivity", "Add selected" );
+                return true;
+            case R.id.action_delete:
+                Log.w( "MainActivity", "Delete selected" );
+                return true;
+            case R.id.action_update:
+                Log.w( "MainActivity", "Update selected" );
+                return true;
+            default:
+                return super.onOptionsItemSelected( item );
+        }
     }
 }
